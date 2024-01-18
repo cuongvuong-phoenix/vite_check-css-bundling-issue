d8cde61e83c0abdccf9a6cdd66c2246f0c208557
Cuong Vuong <cuongvuong.phoenix@gmail.com>
2024-01-18 13:08:03 +0700

    feat: [CORRECT] build ESM only for `lib-a` & import via SCSS in `b`

--- packages/lib-a/vite.config.ts
+++ packages/lib-a/vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
     lib: {
       entry: resolve(__dirname, 'src/main.ts'),
       name: 'libA',
+      formats: ['es'],
       fileName: 'lib-a',
     },
     rollupOptions: {
