cd1b4949231159aecd9b20388e9dd5fcd5c387f5
Cuong Vuong <cuongvuong.phoenix@gmail.com>
2024-01-18 16:42:52 +0700

    feat: [CORRECT] build all variants for `lib-a`

--- packages/lib-a/vite.config.ts
+++ packages/lib-a/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     lib: {
       entry: resolve(__dirname, 'src/main.ts'),
       name: 'libA',
-      formats: ['es'],
+      // formats: ['es'],
       fileName: 'lib-a',
     },
     rollupOptions: {
